2fb65e8ee2348c5b3c8e08744ef25d082839cee0
Ashutosh Sanjeev Bhujbal <theflash257@gmail.com>
2024-03-03 14:22:20 +0000

    add scroll effect to cta

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
-
 const hamEl = document.getElementById("ham")
 const navItemsEl = document.querySelector(".nav-items")
+const ctaButton = document.getElementById("cta-button")
+
 
 hamEl.addEventListener("click", () => {
     navItemsEl.style.display =  (
         navItemsEl.style.display === "none") || (navItemsEl.style.display === "") 
         ? "flex" : "none"
+})
+
+
+
+ctaButton.addEventListener('click', function(e) {
+    e.preventDefault()
+    const targetElement = document.getElementById("cta-section")
+
+    window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+    })
 })
